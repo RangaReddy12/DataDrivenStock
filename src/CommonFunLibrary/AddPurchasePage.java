0624e8666a542878c13c5b437c5fd941783e5de9
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2022-12-28 20:30:17 +0530

    New Methods Added

---
+++ src/CommonFunLibrary/AddPurchasePage.java
@@ -0,0 +1,22 @@
+package CommonFunLibrary;
+
+public class AddPurchasePage {
+public void Add()
+{
+	int a=987,b=98,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=987,b=98,c;
+	c=a*b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=987,b=98,c;
+	c=a/b;
+	System.out.println(c);
+}
+}
